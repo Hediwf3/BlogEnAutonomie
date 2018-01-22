211af0b2f63ba1fba3f2bdc6e73d3820fbcd4fb7
Hediwf3 <32874490+Hediwf3@users.noreply.github.com>
2018-01-22 17:00:59 +0100

    1700

--- assets/php/main.php
+++ assets/php/main.php
@@ -22,7 +22,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
 
     <link rel="stylesheet" href="https://anijs.github.io/lib/anicollection/anicollection.css" />
-  
+
 </head>
 
 
